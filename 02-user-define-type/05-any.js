00a5986b7562d8be46a8ff4b9b4c0bfd9845f5f9
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-16 02:03:53 +0600

    userDefineType  any done

---
+++ 02-user-define-type/05-any.js
@@ -0,0 +1,8 @@
+// any data tpye
+// It should be used when you don't know, what type of input user will give you
+var any;
+any = 1;
+any = 'khan zia';
+any = true;
+any = ['33', 77, true];
+console.log(any);
---
+++ 02-user-define-type/05-any.ts
@@ -0,0 +1,10 @@
+// any data tpye
+
+// It should be used when you don't know, what type of input user will give you
+
+let any : any;
+any=1;
+any='khan zia'
+any=true
+any=['33',77,true]
+console.log(any)
