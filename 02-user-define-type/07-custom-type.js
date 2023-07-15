ff65a152fe5e1b5cd6a8ca9044d8a414a1e31e04
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-16 03:20:46 +0600

    userDefineType done

--- 02-user-define-type/07-custom-type.js
+++ 02-user-define-type/07-custom-type.js
@@ -1,3 +1,4 @@
+// type user = {userName : string , userId?:number} // optional chaining for create option user can put this value or not. Depend on user.
 var users;
 users = [];
 var user1;
@@ -7,3 +8,10 @@ var user2;
 user2 = { userName: 'Megha', userId: 101 };
 users.push(user2);
 console.log(users);
+var getRequest;
+getRequest = "GET";
+// console.log(getReuest)
+function RequestUserValue(requestValue) {
+    console.log(requestValue);
+}
+RequestUserValue("GET");
--- 02-user-define-type/07-custom-type.ts
+++ 02-user-define-type/07-custom-type.ts
@@ -1,3 +1,5 @@
+// type user = {userName : string , userId?:number} // optional chaining for create option user can put this value or not. Depend on user.
+
 type user = {userName : string , userId:number} 
 
 let users : user[];
@@ -12,3 +14,17 @@ user2 = {userName:'Megha', userId:101}
 users.push(user2)
 
 console.log(users)
+
+
+type RequestType = "GET" | "POST"
+
+let getRequest:RequestType;
+getRequest= "GET"
+
+// console.log(getReuest)
+
+function RequestUserValue(requestValue : RequestType){
+    console.log(requestValue)
+}
+
+RequestUserValue("GET")
