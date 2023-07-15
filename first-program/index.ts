d2b297decfa06f7cad7e22eb40c090f066546464
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-15 11:51:43 +0600

    string added

--- first-program/index.ts
+++ first-program/index.ts
@@ -1,1 +1,10 @@
-let a : number ;
+let firstNumber : number ;
+let secondNumber : number ;
+let userFirstName: string;
+let userLastName: string;
+let isEmployee: boolean;
+
+userFirstName = 'khan'
+userLastName = 'zia'
+
+let fullName : string = userFirstName + userLastName;
