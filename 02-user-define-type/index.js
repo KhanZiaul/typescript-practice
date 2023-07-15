0f5a516e0d295fa7b1738eabfd3b23eb15f2ace0
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-15 23:50:42 +0600

    userDefineType union done

--- 02-user-define-type/index.js
+++ 02-user-define-type/index.js
@@ -1,4 +1,11 @@
+// let userId : string | number | boolean ;
 var userId;
 userId = '101';
 userId = 101;
 userId = true;
+function displayUserId(id) {
+    console.log(id);
+}
+displayUserId(101);
+displayUserId('101');
+displayUserId(true);
--- 02-user-define-type/index.ts
+++ 02-user-define-type/index.ts
@@ -1,6 +1,14 @@
-let userId : string | number | boolean ;
+// let userId : string | number | boolean ;
+let userId : (string | number | boolean) ;
 
 userId='101';
 userId=101;
 userId=true;
 
+function displayUserId(id:number|string|boolean){
+    console.log(id)
+}
+
+displayUserId(101)
+displayUserId('101')
+displayUserId(true)
