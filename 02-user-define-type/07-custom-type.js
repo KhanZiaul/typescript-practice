c5fb5f1b13229274a9790ce6fab7b768dbc6d3d6
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-16 03:04:44 +0600

    userDefineType  custom type updated

---
+++ 02-user-define-type/07-custom-type.js
@@ -0,0 +1,9 @@
+var users;
+users = [];
+var user1;
+user1 = { userName: 'khan zia', userId: 102 };
+users.push(user1);
+var user2;
+user2 = { userName: 'Megha', userId: 101 };
+users.push(user2);
+console.log(users);
---
+++ 02-user-define-type/07-custom-type.ts
@@ -0,0 +1,14 @@
+let users : object[];
+users = [];
+
+type user = {userName : string , userId:number} 
+
+let user1 : user
+user1 = {userName:'khan zia', userId:102}
+users.push(user1)
+
+let user2 :user
+user2 = {userName:'Megha', userId:101}
+users.push(user2)
+
+console.log(users)
