7d7a73b6f57b95c9210ad97775470ced375a9215
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-16 01:00:30 +0600

    userDefineType array tuple done

---
+++ 02-user-define-type/tuple-types.js
@@ -0,0 +1,6 @@
+var user;
+user = ['khanzia', 102];
+console.log(user);
+user.push('megha', 40);
+console.log(user);
+console.log(user[1]);
---
+++ 02-user-define-type/tuple-types.ts
@@ -0,0 +1,7 @@
+let user : [string,number];
+user=['khanzia',102]
+console.log(user)
+
+user.push('megha',40)
+console.log(user)
+console.log(user[1])
