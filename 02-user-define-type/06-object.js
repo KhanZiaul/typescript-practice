f8e29cc753fef346ecc9769487f1a1dc94273c1e
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-07-16 02:30:09 +0600

    userDefineType  Normal object done

---
+++ 02-user-define-type/06-object.js
@@ -0,0 +1,15 @@
+// Object 
+// Normal object
+// let user : object;
+// user={Name : 'khan zia'}
+// console.log(user)
+// let user1 : {userName : string , userId:number}
+// user1 = {userName:'khan zia', userId:102}
+// console.log(user1)
+//Array of object
+var user;
+user = [];
+var user1;
+user1 = { userName: 'khan zia', userId: 102 };
+var user2;
+user1 = { userName: 'Megha', userId: 101 };
---
+++ 02-user-define-type/06-object.ts
@@ -0,0 +1,24 @@
+// Object 
+
+// Normal object
+
+// let user : object;
+// user={Name : 'khan zia'}
+// console.log(user)
+
+// let user1 : {userName : string , userId:number}
+// user1 = {userName:'khan zia', userId:102}
+// console.log(user1)
+
+
+
+//Array of object
+
+let user : object[];
+user = [];
+
+let user1 : {userName : string , userId:number}
+user1 = {userName:'khan zia', userId:102}
+
+let user2 : {userName : string , userId:number}
+user1 = {userName:'Megha', userId:101}
